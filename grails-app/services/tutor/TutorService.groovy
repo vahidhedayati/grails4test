acc8228f1363220e697bb04de778520e9ee0ce71
V Hedayati <badvad@gmail.com>
2021-03-13 17:56:20 +0000

    Tutor / Student methods working - JSON results ready for a front end

--- grails-app/services/tutor/TutorService.groovy
+++ grails-app/services/tutor/TutorService.groovy
@@ -26,12 +26,9 @@ abstract class TutorService implements TutorInterfaceService {
         def executedQuery = StudentCourse.executeQuery(query,[courses:tutor.courses],[ readOnly:true])
         Map<Course,List<StudentCourse>> res1 =  executedQuery.groupBy{ StudentCourse sc -> sc.course }
         Map<Course,List<Student>> results = [:]
-        println "-- ${res1} @@"
         res1?.each {k,v ->
-            println "--- ${k} vs ${v.student}"
             results."${k}" = v.student
         }
-        println "-- returns is =  ${results}"
         return results
     }
 }
